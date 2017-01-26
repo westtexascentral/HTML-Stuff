e49df071350d9cd192493974a3ced062b00fdebc
August Whitlock <westtexascentral@gmail.com>
2017-01-26 10:24:31 -0600

    Added an About Section.

---
+++ content-with-hidden-tabs/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<meta name="description" content="Creative One Page Parallax Template">
+	<meta name="keywords" content="Creative, Onepage, Parallax, HTML5, Bootstrap, Popular, custom, personal, portfolio" /> 
+	<meta name="author" content=""> 
+	<title>HIMU - OnePage HTML Parallax template</title> 
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/prettyPhoto.css" rel="stylesheet"> 
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet"> 
+	<link href="assets/css/animate.css" rel="stylesheet"> 
+	<link href="assets/css/main.css" rel="stylesheet">
+	<link href="assets/css/responsive.css" rel="stylesheet"> 
+
+	<link href="style.css" rel="stylesheet"> 
+	<!--[if lt IE 9]> <script src="js/html5shiv.js"></script> 
+	<script src="js/respond.min.js"></script> <![endif]--> 
+	<link rel="shortcut icon" href="images/ico/favicon.png"> 
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png"> 
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png"> 
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png"> 
+	<link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+<body>
+
+<section id="about-us">
+	<div class="container">
+		<div class="text-center">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2 class="title-one">Why With Us?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+			</div>
+		</div>
+		<div class="about-us">
+			<div class="row">
+				<div class="col-sm-6">
+					<h3>Why with us?</h3>
+					<ul class="nav nav-tabs">
+						<li class="active"><a href="#about" data-toggle="tab"><i class="fa fa-chain-broken"></i> About</a></li>
+						<li><a href="#mission" data-toggle="tab"><i class="fa fa-th-large"></i> Mission</a></li>
+						<li><a href="#community" data-toggle="tab"><i class="fa fa-users"></i> Community</a></li>
+					</ul>
+					<div class="tab-content">
+						<div class="tab-pane fade in active" id="about">
+							<div class="media">
+								<img class="pull-left media-object" src="images/about-us/about.jpg" alt="about us"> 
+								<div class="media-body">
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+								</div>
+							</div>
+						</div>
+						<div class="tab-pane fade" id="mission">
+							<div class="media">
+								<img class="pull-left media-object" src="images/about-us/mission.jpg" alt="Mission"> 
+								<div class="media-body">
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci </p>
+								</div>
+							</div>
+						</div>
+						<div class="tab-pane fade" id="community">
+							<div class="media">
+								<img class="pull-left media-object" src="images/about-us/community.jpg" alt="Community"> 
+								<div class="media-body">
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<h3>Our Skills</h3>
+					<div class="skill-bar">
+						<div class="skillbar clearfix " data-percent="90%">
+							<div class="skillbar-title">
+								<span>HTML5 &amp; CSS3</span>
+							</div>
+							<div class="skillbar-bar"></div>
+							<div class="skill-bar-percent">90%</div>
+						</div> <!-- End Skill Bar -->
+						<div class="skillbar clearfix" data-percent="85%">
+							<div class="skillbar-title"><span>UI Design</span></div>
+							<div class="skillbar-bar"></div>
+							<div class="skill-bar-percent">85%</div>
+						</div> <!-- End Skill Bar -->
+						<div class="skillbar clearfix " data-percent="70%">
+							<div class="skillbar-title"><span>jQuery</span></div>
+							<div class="skillbar-bar"></div>
+							<div class="skill-bar-percent">70%</div>
+						</div> <!-- End Skill Bar -->
+						<div class="skillbar clearfix " data-percent="60%">
+							<div class="skillbar-title"><span>PHP</span></div>
+							<div class="skillbar-bar"></div>
+							<div class="skill-bar-percent">60%</div>
+						</div> <!-- End Skill Bar -->
+						<div class="skillbar clearfix " data-percent="75%">
+							<div class="skillbar-title"><span>Wordpress</span></div>
+							<div class="skillbar-bar"></div>
+							<div class="skill-bar-percent">75%</div>
+						</div> <!-- End Skill Bar --></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!--/#about-us-->
+
+<script type="text/javascript" src="assets/js/jquery.js"></script> 
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/smoothscroll.js"></script> 
+	<script type="text/javascript" src="assets/js/jquery.isotope.min.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.prettyPhoto.js"></script> 
+	<script type="text/javascript" src="assets/js/jquery.parallax.js"></script> 
+	<script type="text/javascript" src="assets/js/main.js"></script> 
+
+	<script type="text/javascript" src="app.js"></script>
+</body>
+</html>
